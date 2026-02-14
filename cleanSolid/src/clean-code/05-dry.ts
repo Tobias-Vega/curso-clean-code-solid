a6b3895aed68b49e5b020bdb4a999b112f7178cc
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-14 17:02:02 -0300

    Aplicando DRY

--- cleanSolid/src/clean-code/05-dry.ts
+++ cleanSolid/src/clean-code/05-dry.ts
@@ -6,16 +6,31 @@ class Product {
     public name: string = '',
     public price: number = 0,
     public size: Size = '',
-  ) {}
+  ) { }
+
+  isProductReady(): boolean {
+
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case 'string':
+          if (this[key].length <= 0) throw Error(`${key} is empty`);
+          break;
+        case 'number':
+          if (this[key] <= 0) throw Error(`${key} is zero`)
+          break;
+        default:
+          throw Error(`${typeof this[key]} is not valid`)
+      }
+    }
+
+    return true;
+
+  }
 
 
   toString() {
 
-    // No DRY
-
-    if (this.name.length <= 0) throw Error('Name is empty');
-    if (this.price <= 0) throw Error('Price is zero');
-    if (this.size.length <= 0) throw Error('Size is empty');
+    if (!this.isProductReady()) return;
 
     return `${this.name} (${this.price}), ${this.size}`;
   }
@@ -27,6 +42,6 @@ class Product {
   const bluePants = new Product('Blue Large Pants', 10, 'M');
 
   console.log(bluePants.toString());
-  
+
 
 })()
