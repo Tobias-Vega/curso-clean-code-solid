bd3af6e25c8a426a96031eaeaf29d8bd4b271820
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-14 13:41:44 -0300

    Refactorización sobre funciones

--- cleanSolid/src/clean-code/03-functions.ts
+++ cleanSolid/src/clean-code/03-functions.ts
@@ -1,46 +1,59 @@
 (() => {
 
-    function getMovieById( id: string ) {
-        console.log({ id });
+  function getMovieById(id: string) {
+    console.log({ id });
+  }
+
+  function getMovieCastById(id: string) {
+    console.log({ id });
+  }
+
+  function getActorBioById(id: string) {
+    console.log({ id });
+  }
+
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  function checkFullNme(fullname: string) {
+    console.log({ fullname });
+    return true;
+  }
+
+  function createActor(fullName: string, birthdate: Date): boolean {
+
+    // tarea asincrona para verificar nombre
+    // ..
+    // ..
+    if (checkFullNme(fullName)) return false;
+
+    console.log('Crear actor', birthdate);
+    return true;
+
+  }
+
+  // Continuar
+
+  const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+
+
+    if (isDead) {
+      return 1500;
     }
 
-    function getMovieCastById( id: string ) {
-        console.log({ id });
+    if (isSeparated) {
+      return 2500;
     }
 
-    function getActorBioById( id: string ) {
-        console.log({ id });
-    }
-
-    interface Movie {
-      cast: string[];
-      description: string;
-      rating: number;
-      title: string;
-    }
-    
-    function createMovie({ title, description, rating, cast }: Movie) {
-        console.log({ title, description, rating, cast });
-    }
-
-    function checkFullNme(fullname: string) {
-      console.log({fullname});
-      return true;
-    }
-
-    function createActor( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( checkFullNme(fullName) ) return false;
-
-        console.log('Crear actor', birthdate);
-        return true;        
-
-    }
-
-    
-
+    return (isRetired) ? 3000 : 4000;
+  }
 
 })();
