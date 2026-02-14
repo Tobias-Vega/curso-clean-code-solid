39fbb7cf8a843a379cac846cd8317464070f4e5d
Tobias Vega <tobiasvega1210@gmail.com>
2026-02-14 17:35:48 -0300

    Herencia - Problemática

--- cleanSolid/src/clean-code/06-classes-a.ts
+++ cleanSolid/src/clean-code/06-classes-a.ts
@@ -1,10 +1,10 @@
 (() => {
 
+  // No aplicando el principio de responsabilidad única
+
   type Gender = 'M' | 'F';
 
   class Person {
-
-
     constructor(
       public name: string,
       public gender: Gender,
@@ -12,8 +12,52 @@
     ) {}
   }
 
-  const newPerson = new Person('Tobías', 'M', new Date('2005-10-13'));
-  console.log({ newPerson });
+  class User extends Person {
+
+    public lastAccess: Date;
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ){
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+
+      super(email, role, name, gender, birthdate)
+
+    }
+  }
+
+  const userSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'tobias@gmail.com',
+    'Admin',
+    'Tobías',
+    'M',
+    new Date('2005-10-13')
+  );
+
+  console.log({ userSettings })
 
 
 
